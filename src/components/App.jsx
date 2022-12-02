ce826369624495692161ffacd7250085c22bb969
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-12-02 21:14:40 +0200

    -

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,15 @@
 import { Component } from 'react';
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+  ul,h1,h2,h3,h4,h5,h6,li{list-style:none;margin:0;padding:0;};
+  body{
+    height:100vh;
+   display: flex;
+   justify-content:center;
+   align-items:center;
+   color: '#010101'; 
+  }
+`;
 export class App extends Component {
   state = {
     good: 0,
@@ -7,17 +18,8 @@ export class App extends Component {
   };
   render() {
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        km
+      <div>
+        <GlobalStyle />
       </div>
     );
   }
